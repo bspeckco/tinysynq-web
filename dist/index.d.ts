061865e8988872d74a2d61d1dee72109170dd10f
lnoir <gh@latn.black>
2024-03-03 17:50:32 +0000

    docs: API documentation

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 import lib from './lib/index.js';
 export default lib;
+export type { SyncableTable, SynQLiteOptions, SynQLiteOptionsBase, Change, QueryParams, SQLiteWASM, } from './lib/types.js';
+export type { SynQLite, GetTableIdColumnParams } from './lib/synqlite.class.js';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import lib from './lib/index.js';
 
 export default lib;
+
+export type { 
+  SyncableTable,
+  SynQLiteOptions,
+  SynQLiteOptionsBase,
+  Change,
+  QueryParams,
+  SQLiteWASM,
+} from './lib/types.js';
+
+export type {
+  SynQLite,
+  GetTableIdColumnParams
+} from './lib/synqlite.class.js';
